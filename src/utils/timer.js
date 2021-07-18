22730319cb8e41aafbe8dc23b49f5087de8a842c
TankistPro <nakol.nayk@mail.ru>
2021-07-18 17:06:05 +0300

    fixed timer bug

--- src/utils/timer.js
+++ src/utils/timer.js
@@ -1,26 +1,30 @@
 class Timer {
     constructor() {
-        this.seconds = 0;
-        this.minutes = 0;
+        this.seconds = 50;
+        this.minutes = 59;
         this.hours = 0;
     }
     
+    timeFormat(time) {
+        return time < 10 ? "0" + time : time;
+    }
+
     displayTimer() {
-        return `${this.hours}:${this.minutes < 10 ? "0" + this.minutes : this.minutes}:${this.seconds < 10 ? "0" + this.seconds: this.seconds}`
+        return `${this.timeFormat(this.hours)}:${this.timeFormat(this.minutes)}:${this.timeFormat(this.seconds)}`;
     }
 
     startWork() {
-        if(this.seconds < 59) {
-            this.seconds++;
-        } else {
+        if(this.seconds < 59) this.seconds++;
+        else {
             this.seconds = 0;
             this.minutes++;
         }
         if(this.minutes === 60) {
             this.hours++;
+            this.minutes = 0;
         }
 
-        return this.displayTimer()
+        return this.displayTimer();
     }
 
     pauseWork(timerID) {
