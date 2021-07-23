142e949a39c19522301f31d514a0f55a684e1e10
TankistPro <nakol.nayk@mail.ru>
2021-07-23 14:29:32 +0300

    fix

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -7,10 +7,10 @@ module.exports.isAuth = async(ctx, next) => {
 }
 
 module.exports.updateDate = async (ctx, next) => {
-    if (state.userID && ctx.session?.userData === undefined) {
+    if (ctx.session?.userData === undefined) {
         const userData = await userModel.findOne({id_user: state.userID});
         
-        ctx.session.userData = userData;
+        if (userData) ctx.session.userData = userData;
     }
     await next();
 }
